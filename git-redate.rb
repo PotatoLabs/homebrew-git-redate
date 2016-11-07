0c2a59d98c998c11b9efee817276c83739e47994
Edmund Mai <edmundm@crowdtap.com>
2016-11-07 10:10:06 -0500

    Update to v1.0.1 - Fixes tmpfile error

--- git-redate.rb
+++ git-redate.rb
@@ -1,9 +1,9 @@
 class GitRedate < Formula
   desc "Change the dates of several git commits with a single command"
   homepage "https://github.com/PotatoLabs/git-redate"
-  url "https://github.com/PotatoLabs/git-redate/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "6bab90cfa469e2d9170adb79f21ad9e6f79c55e7e212cd87d866bfb12887f9da"
+  url "https://github.com/PotatoLabs/git-redate/archive/1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "337cd2d17d7fcce5f68557dd6bb9ca03c5d5543c313c50e4dff93d1d23c0e6f6"
 
   def install
     bin.install "git-redate"
