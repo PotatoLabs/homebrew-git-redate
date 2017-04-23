964a917f40838ad7cc2b15d818bd8b6f1eefbd5b
Edmund Mai <edmundmai@gmail.com>
2017-04-23 15:02:17 +0900

    update to v1.0.2

--- git-redate.rb
+++ git-redate.rb
@@ -1,9 +1,9 @@
 class GitRedate < Formula
   desc "Change the dates of several git commits with a single command"
   homepage "https://github.com/PotatoLabs/git-redate"
-  url "https://github.com/PotatoLabs/git-redate/archive/1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "337cd2d17d7fcce5f68557dd6bb9ca03c5d5543c313c50e4dff93d1d23c0e6f6"
+  url "https://github.com/PotatoLabs/git-redate/archive/1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "0e82cf08ba8a2c73521f150b17b76ea00f87543faa9ebfa72833234ab0703e3d"
 
   def install
     bin.install "git-redate"
